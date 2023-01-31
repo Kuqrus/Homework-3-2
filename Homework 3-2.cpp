db32eec4126f7550c641ded6649fda2f91d10450
Nikolay Pozharskikh <kuqrus@gmail.com>
2023-01-31 17:20:08 +0500

    Добавьте файлы проекта.

---
+++ Homework 3-2.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+
+class counter
+{
+public:
+
+	void setUserValue(int num_) {
+		num = num_;
+	}
+	void setDefaultValue() {
+		num = 1;
+	}
+
+	int add() { return num++; }
+	int sub() { return num--; }
+	void print() { std::cout << "Current value: " << num << std::endl; }
+
+private:
+	int num;
+};
+
+
+int main()
+{
+	counter test;
+
+	char isUserValue = ' ';
+	
+	std::cout << "Do you want to input start value (y/n)? ";
+	std::cin >> isUserValue;
+	
+	if (isUserValue == 'y') {
+		int userValue = 0;
+		std::cout << "Enter start value: ";
+		std::cin >> userValue;
+		test.setUserValue(userValue);
+	}
+	else if (isUserValue == 'n') {
+		test.setDefaultValue();
+	}	
+
+
+	char userAction= ' ';
+	while(userAction != 'x')
+	{
+		std::cout << "Choose action (+, -, =, x): ";
+		std::cin >> userAction;
+
+		switch (userAction)
+		{
+		case('+'):
+			test.add();
+			break;
+		case('-'):
+			test.sub();
+			break;
+		case('='):
+			test.print();
+			break;
+		default:
+			break;
+		}
+
+	}
+	
+
+}
+
